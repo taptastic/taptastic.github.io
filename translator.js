1ebe4613d0565bd29b298bd114e997ebf7c18060
Barry Hong <hunghongphugia@gmail.com>
2017-01-15 06:52:55 -0500

    Longitudinal I hate you

--- translator.js
+++ translator.js
@@ -34,7 +34,7 @@ function peakAnalysis() {
 	// motion values: 0 for stationary, 1 for right, -1 for left
 	for (var i = 1; i < indexMPArray.length; i++) {
 		// Not the same peak, meaning there is some transition in peak => Longitudinal
-		if ((indexMPArray[i] != indexMPArray[i-1]) && (Math.abs(indexMPArray[i] - indexMPArray[i-1]) > 4)) {
+		if ((indexMPArray[i] != indexMPArray[i-1]) && (Math.abs(indexMPArray[i] - indexMPArray[i-1]) > 6)) {
 			// Record time frame in which the peak transit
 			mpLongitudinal.push(i);
 			// Record direction of travel
