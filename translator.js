a315d3b481e24c31b2a737cc1151b013bb12bb81
Barry Hong <hunghongphugia@gmail.com>
2017-01-14 21:03:45 -0500

    Update translator,js: Addon to code

--- translator.js
+++ translator.js
@@ -6,22 +6,46 @@ function peakCollection() {
     var mainArray = frequencyData.slice(8);
 
     // Find peak from the bassArray, locating it as the foundation peak
-	var initialBP = Math.max.apply(Math, bassArray);
+	var valueBP = Math.max.apply(Math, bassArray);
 	var indexBP = bassArray.indexOf(initialBP);
 
 	// Find peak from the mainArray, locating it as the foundation peak
-	var initialMP = Math.max.apply(Math, mainArray);
+	var valueMP = Math.max.apply(Math, mainArray);
 	var indexMP = bassArray.indexOf(initialMP);
 
 	// Append the values found into the arrays that we will be looking and comparing for behaviours.
-	initialBPArray.push(initialBP);
+	valueBPArray.push(initialBP);
 	indexBPArray.push(indexBP);
-    initialMPArray.push(initialBP);
+    valueMPArray.push(initialBP);
     indexMPArray.push(indexBP);
 }
 
 // Understand the peak data and translate them into valid information
 function peakAnalysis() {
 	// Observing and comparing for trend / motion
+
+	// Working with Main part first
+	// motionCheck: 0 for stationary, 1 for right, -1 for left
+	var mpLongitudinal = [];
+	var mpMotion = [];
+	for (var i = 1; i < indexMPArray.length; i++) {
+		// Not the same peak, meaning there is some transition in peak => Transverse
+		if (indexMPArray[i] != indexMPArray[i-1]) {
+			// Record position in which the peak transit
+			mpLongitudinal.push(indexMPArray[i-1]);
+			// Record direction of travel
+			if (indexMPArray[i] > indexMPArray[i-1]) {
+				mpMotion.push(1);
+			} else mpMotion.push(-1);
+		} else mpMotion.push(0);
+    }
+    // Seeking for longitudinal motion: a significant change in value of highest peak
+    var mpTransverse = [];
+	for (var i = 1; i < valueMPArray.lengthh; i++) {
+		// Check for sudden increase in value of peak. Value used: 32
+		if (valueMPArray[i] - valueMPArray[] >= 32){
+
+		}
+	}
 
 }
